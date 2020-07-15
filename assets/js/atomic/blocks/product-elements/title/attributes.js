31077d6cf4a09b81b3c04e2cde9c20654a96e80e
Mike Jolley <mike.jolley@me.com>
2020-07-15 12:36:38 +0100

    Product selection when out of context for price and title blocks

---
+++ assets/js/atomic/blocks/product-elements/title/attributes.js
@@ -0,0 +1,16 @@
+export const blockAttributes = {
+	headingLevel: {
+		type: 'number',
+		default: 2,
+	},
+	productLink: {
+		type: 'boolean',
+		default: true,
+	},
+	productId: {
+		type: 'number',
+		default: 0,
+	},
+};
+
+export default blockAttributes;
